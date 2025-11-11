28f2d1fbcd980e93a597f7f5863fc4b8962a3726
dnicodeguzman <dannicodguzman@gmail.com>
2025-11-11 23:01:33 +0800

    Initial commit

---
+++ src/pages/NotFound/NotFound.jsx
@@ -0,0 +1,12 @@
+import "./not-found.css";
+
+function NotFound() {
+    return (
+        <> 
+            <h1> NOT FOUND! ERROR! </h1>
+            <p> This page does not exist! </p>
+        </>
+    )
+}
+
+export default NotFound;
